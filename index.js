9425c544145c38eebcc494ea47ca17e593796422
faiqus <faiqrazarizvi@gmail.com>
2014-09-14 06:22:34 -0400

    twitter auth

--- index.js
+++ index.js
@@ -4,20 +4,40 @@ var express = require('express')
   , path = require('path')
   , http = require('http')
   , app = express()
-  , server = http.createServer(app)
+  , twitterA = require('node-twitter-api') 
+  , twitter = new twitterA({
+    consumerKey: 'RR1DhG4BzaZXbo5Zs2ZAJ4omV',
+    consumerSecret: '2cR9Hi24oddtg9WvC8nKJ9WWdt4T89KjspeakxvY5g2NSOMd4k',
+    callback: 'http://richardstallman.me/post'
+  })
+  , requestTokenG = null 
+  , requestTokenSecretG = null
+  , server = http.createServer(app);
 
-app.use(bodyParser.json())
+app.use(logger('dev'));
+app.set('views', './views');
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
   extended: true
-}))
-app.use(logger('dev'))
-app.set('views', './views')
-app.use(express.static(__dirname + '/public'));
+}));
 
-app.get('/', function (req, res){
-  res.sendfile('./views/index.html')
+twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+    if (error) console.log("Error getting OAuth request token : " + error);
+    else {
+      requestTokenG = requestToken; 
+      requestTokenSecretG = requestTokenSecret; 
+    }
 });
 
+
+app.get('/', function (req, res){ 
+  res.sendfile('./views/index.html');
+});
+
+app.get('/post', function(req, res){ 
+  res.sendfile('./views/post-stat.html');
+}); 
 server.listen(4006, function(){
   console.log('listening on *:4006');
 });

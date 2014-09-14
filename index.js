2f8e1f61f060cd60535346b6f1015610368f993d
faiqus <faiqrazarizvi@gmail.com>
2014-09-14 04:15:21 -0400

    i hate mylife

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express')
+  , logger = require('morgan')
+  , bodyParser = require('body-parser') 
+  , path = require('path')
+  , http = require('http')
+  , app = express()
+  , server = http.createServer(app)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(logger('dev'))
+app.set('views', './views')
+
+app.get('/', function (req, res){ 
+  res.sendfile('./views/index.html')
+});
+
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+}); 

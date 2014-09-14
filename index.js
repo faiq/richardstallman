c108b924bee4967b70fc8ff9b80f52c9451dc60c
faiqus <faiqrazarizvi@gmail.com>
2014-09-14 08:26:57 -0400

    g

--- index.js
+++ index.js
@@ -43,7 +43,12 @@ app.get('/login', function (req, res){
 });
 
 app.get('/tweet', function(req, res){
+  //console
   res.render('post-stat',{quotes: rms});
+});
+
+app.post('/post', function (req, res){ 
+    
 });
 
 server.listen(4006, function(){
